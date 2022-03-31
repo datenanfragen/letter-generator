b3f881c27ebe8b9f264a667b0d68ab9f881ce327
Lorenz Sieben <me@lorenz-sieben.com>
2022-03-31 23:05:47 +0200

    Export LetterProps type

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { Template } from './Template';
 export { Letter } from './Letter';
 export { PdfRenderer } from './PdfRenderer';
+export type { LetterProps } from './Letter';
 
 export * as util from './utility';
 export * from './utility';
