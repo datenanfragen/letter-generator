d6c3d3ef36a85d8b6af2fafc6b865f4238bb90f1
Benjamin Altpeter <benjamin@oldpeter.de>
2022-03-31 21:22:26 +0000

    Update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export { Template } from './Template';
 export { Letter } from './Letter';
 export { PdfRenderer } from './PdfRenderer';
 export type { LetterProps } from './Letter';
-export type { LayoutFunction, Signature } from './types.d';
+export type { LayoutFunction, Signature } from './types';
 
 export * as util from './utility';
 export * from './utility';
