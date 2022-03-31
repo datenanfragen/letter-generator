8c228ddb9c5b28d87578c641ddd7e7bb8670b3fc
Lorenz Sieben <me@lorenz-sieben.com>
2022-03-31 23:14:35 +0200

    Expose more types

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { Template } from './Template';
 export { Letter } from './Letter';
 export { PdfRenderer } from './PdfRenderer';
 export type { LetterProps } from './Letter';
+export type { LayoutFunction, Signature } from './types.d';
 
 export * as util from './utility';
 export * from './utility';
