2281a47d095be35d61314f137b09037901d414be
Benjamin Altpeter <benjamin@oldpeter.de>
2022-07-19 11:33:18 +0200

    Introduce auto-set 'has:var_name' flags

--- src/Template.ts
+++ src/Template.ts
@@ -1,3 +1,5 @@
+import { matchAll } from './utility';
+
 export class Template {
     template: string;
     flags: Record<string, boolean>;
@@ -11,6 +13,9 @@ export class Template {
      *       This will be replaced by the value of the variable `var_name`.
      * @param flags An object that maps the flags in the template to a boolean, e.g.:
      *     `{flag1: true, flag2: false}`
+     *
+     *     Unless overridden, for each variable `var_name`, a flag `has:var_name` will be added automatically and set to
+     *     whether the variable is set and non-empty.
      * @param variables An object that variables in the template to their value, e.g.:
      *      `{var1: 'Johnny', var2: 'Hello world.'}`
      */
@@ -24,12 +29,28 @@ export class Template {
      */
     getText(): string {
         let result = this.template;
+
+        const replace_flag = (text: string, flag_name: string, flag_value: boolean) =>
+            text.replace(new RegExp('\\[' + flag_name + '>([\\s\\S]*?)\\]', 'gmu'), flag_value ? '$1' : '');
+        // Replace the "regular" flags set by the user.
         for (const flag in this.flags) {
-            result = result.replace(new RegExp('\\[' + flag + '>([\\s\\S]*?)\\]', 'gmu'), this.flags[flag] ? '$1' : '');
+            result = replace_flag(result, flag, this.flags[flag]);
         }
+        // Replace the automatically generated `has:var_name` flags. As these are handled after the regular flags, this
+        // will not override any user-defined flags of the same name.
+        for (const [, has_flag_name, var_name_to_check] of matchAll(/\[(has:(.+?))>[\s\S]*?\]/gmu, result)) {
+            result = replace_flag(
+                result,
+                has_flag_name,
+                !!this.variables[var_name_to_check] && this.variables[var_name_to_check] !== ''
+            );
+        }
+
+        // Replace the variables set by the user.
         for (const variable in this.variables) {
             result = result.replace('{' + variable + '}', this.variables[variable]);
         }
+
         return result;
     }
 }
